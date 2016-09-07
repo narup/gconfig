fccb1f7dfb49043a1936f760ecfd8588e146be98
Puran Singh <puran@Purans-MacBook-Pro.local>
2016-09-06 19:39:01 -0700

    first implementation complete

--- gconfig.go
+++ gconfig.go
@@ -1,36 +1,108 @@
-//spring boot style configuration managemer
+// Package gconfig - Spring boot style configuration manager. It can load either properties or yaml formatted files.
+// properties file should follow the naming convention:
+//
+// 1. application.properties: this holds all the default configuration values as key/value pair.
+// 2. application-{profile}.properties. contains all the environment specific configuration values.
+//    eg: for prod environment, application-prod.properties
+
 package gconfig
 
 import (
 	"bufio"
 	"flag"
-	"github.com/pkg/errors"
-	"io/ioutil"
-	"os"
-	s "strings"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	s "strings"
+
+	"github.com/pkg/errors"
+	"path/filepath"
+	"strconv"
 )
 
-type GConfig struct {
-	Profile  string
-	FileName string
+const (
+	PROP_EXTENSION         string = ".properties"
+	DEFAULT_PROP_FILENAME  string = "application-default.properties"
+	STANDARD_PROP_FILENAME string = "application.properties"
+)
+
+var Gcg *GConfig
+var ConfigFileRequired = errors.New("At least one configuration file is required")
+
+// configFile is a internal representation of individual configurations for default and env specific
+// configuration values.
+type configFile struct {
+	fileInfo os.FileInfo
 	configs  map[string]interface{}
 }
 
-func (c *GConfig) GetString(key string) string {
-	return ""
+func (cf configFile) Name() string {
+	return cf.fileInfo.Name()
 }
 
-func (c *GConfig) GetInt(key string) int {
-	return 0
+func (cf *configFile) addProperty(key, value string) {
+	k := s.Trim(key, " ")
+	v := s.Trim(value, " ")
+
+	cf.configs[k] = v
 }
 
-func (c *GConfig) GetBool(key string) string {
-	return ""
+func (cf configFile) isDefault() bool {
+	if cf.Name() == DEFAULT_PROP_FILENAME || cf.Name() == STANDARD_PROP_FILENAME {
+		return true
+	}
+	return false
 }
 
-func (c *GConfig) PrettyPrint(key string) string {
-	return ""
+// GConfig is the representation of all the configuration properties. It loads 2 types of data: default and environment
+// specific. One out of 2 must be present otherwise, error is returned during the Load operation
+type GConfig struct {
+	Profile                      string
+	profileConfig, defaultConfig configFile
+}
+
+func (c GConfig) GetString(key string) string {
+	return c.getValue(key)
+}
+
+func (c GConfig) GetInt(key string) int {
+	i, _ := strconv.Atoi(c.getValue(key))
+	return i
+}
+
+func (c GConfig) GetFloat(key string) float64 {
+	v, _ := strconv.ParseFloat(c.getValue(key), 32)
+	return v
+}
+
+func (c GConfig) GetBool(key string) bool {
+	b, _ := strconv.ParseBool(c.getValue(key))
+	return b
+}
+
+// getValue returns a value for a given key as type interface which is converted to actual return type by
+// individual Get* functions.
+func (c GConfig) getValue(key string) string {
+	v := c.profileConfig.configs[key]
+	if v == nil {
+		v = c.defaultConfig.configs[key]
+	}
+
+	return v.(string)
+}
+
+func (c *GConfig) addConfigFile(cf configFile) {
+	if cf.isDefault() {
+		c.defaultConfig = cf
+	} else {
+		c.profileConfig = cf
+	}
+}
+
+func (c GConfig) isEmpty() bool {
+	return len(c.profileConfig.configs) == 0 && len(c.defaultConfig.configs) == 0
 }
 
 func configError(cause error, format string, args ...interface{}) (*GConfig, error) {
@@ -39,44 +111,67 @@ func configError(cause error, format string, args ...interface{}) (*GConfig, err
 
 func Load() (*GConfig, error) {
 	gc := new(GConfig)
-	gc.configs = make(map[string]interface{})
 	gc.Profile = loadProfile()
-	gc.FileName = "application-" + gc.Profile + ".properties"
 
 	p := loadPath()
 	files, err := ioutil.ReadDir(p)
 	if err != nil {
-		return configError(err, "Error reading config directory at path:: %s", p)
+		return configError(err, "Error reading config directory in path %s", p)
+	}
+	if len(files) == 0 {
+		return configError(ConfigFileRequired, "Config file not found in path %s", p)
 	}
 
-	fis := make([]os.FileInfo, 2)
-	for _, file := range files {
-		if file.Name() == "application.properties" || file.Name() == gc.FileName {
-			fis = append(fis, file)
+	//read individual config file
+	for _, f := range files {
+		cfpath := filepath.Join(p, f.Name())
+		if path.Ext(f.Name()) == PROP_EXTENSION {
+			cf, err := readPropertyFile(f, cfpath)
+			if err != nil {
+				return configError(err, "Error opening config file %s", f)
+			}
+			gc.addConfigFile(cf)
 		}
 	}
 
-	for _, fi := range files {
-		f, err := os.Open(fi.Name())
-		if err != nil {
-			return configError(err, "Error opening config file:: %s", fi)
-		}
+	Gcg = gc
 
-		sc := bufio.NewScanner(f)
-		sc.Split(bufio.ScanLines)
-		for sc.Scan() {
-			l := sc.Text()
-			kv := s.Split(l, "=")
-			fmt.Printf("Key:%s\n", kv[0])
-			fmt.Printf("Value:%s\n", kv[1])
-
-		}
-
+	//do a final check if loaded config has any values
+	if gc.isEmpty() {
+		log.Printf("Configuration loaded, but empty for profile: '%s'\n", Gcg.Profile)
+	} else {
+		log.Printf("Configuration loaded for profile %s\n", Gcg.Profile)
 	}
+
 	return gc, nil
 }
 
-//If no profile is specified then it uses the default profile and load the config data that's marked as default.
+// readPropertyFile opens the configuration file and creates configuration struct with all the key/value pair info.
+// It ignores any line that begins with # and silently ignores line without correct key/value pair format.
+func readPropertyFile(fi os.FileInfo, cfpath string) (configFile, error) {
+	cf := configFile{fileInfo: fi, configs: make(map[string]interface{})}
+
+	f, err := os.Open(cfpath)
+	if err != nil {
+		return configFile{}, err
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+	sc.Split(bufio.ScanLines)
+	for sc.Scan() {
+		l := sc.Text()
+		kv := s.Split(l, "=")
+		if len(kv) == 2 {
+			cf.addProperty(kv[0], kv[1])
+		}
+	}
+
+	return cf, nil
+}
+
+// If no profile is specified then it uses the default profile and load the config
+// data that's marked as default.
 // Profile can be set using 2 ways:
 // 1. Environment variable 'GC_PROFILE' eg: export GC_PROFILE='dev'
 // 2. Command line argument 'profile' eg: go run myserver.go -profile=dev
